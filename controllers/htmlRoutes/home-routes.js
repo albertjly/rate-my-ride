c80f56580c8272dcb9b91b8857d6887f41d386d3
Albert Jiang <albertjly@gmail.com>
2021-05-26 21:33:51 -0700

    routes changes

--- controllers/htmlRoutes/home-routes.js
+++ controllers/htmlRoutes/home-routes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 
-const sequelize = require('../config/connection');
-const { Car, User, Comment, } = require('../models');
+const sequelize = require('../../config/connection');
+const { Car, User, Comment, } = require('../../models');
 
 // get all posts for homepage
 router.get('/', (req, res) => {
   console.log('======================');
   Car.findAll({
-    attributes: ['id', 'make', 'model', 'year', 'color', 'description', 'user_id', 'created_at' ],
+    attributes: ['id', 'make', 'model', 'year', 'color', 'description', 'user_id', 'created_at'],
     include: [
       {
         model: Comment,
@@ -43,7 +43,7 @@ router.get('/car/:id', (req, res) => {
     where: {
       id: req.params.id
     },
-    attributes: ['id', 'make', 'model', 'year', 'color', 'description', 'user_id', 'created_at' ],
+    attributes: ['id', 'make', 'model', 'year', 'color', 'description', 'user_id', 'created_at'],
     include: [
       {
         model: Comment,
